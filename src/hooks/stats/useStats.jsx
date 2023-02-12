30f308b7b213378a719ae4be7b8fa900737aedbf
Angel Gonzalez <angele.gm91@gmail.com>
2023-02-12 17:57:08 -0300

    (improv) show only the accumulated date for the selected date in the summary view

--- src/hooks/stats/useStats.jsx
+++ src/hooks/stats/useStats.jsx
@@ -2,13 +2,11 @@ import dayjs from "dayjs";
 import formatDuration from "format-duration";
 import { times } from "lodash";
 import { useCallback } from "react";
-import { useMemo } from "react"
-import { useTasks } from "../tasks/useTasks"
+import { useMemo } from "react";
+import { useTasks } from "../tasks/useTasks";
 
 export const useStats = () => {
-  const {
-    tasks,
-  } = useTasks();
+  const { tasks } = useTasks();
 
   const allHistory = useMemo(() => {
     return tasks.list.reduce((acc, task) => {
@@ -24,65 +22,77 @@ export const useStats = () => {
     return historyItemsThisUnit.reduce((acc, historyItem) => {
       return acc + historyItem.duration;
     }, 0);
-  }
+  };
 
   const totalTimeThisMonth = useMemo(() => {
     const totalTimeThisMonth = totalTimeByUnitAndDate({
-      unit: 'month',
+      unit: "month",
       history: allHistory,
       date: new Date(),
     });
 
     return formatDuration(totalTimeThisMonth * 1000);
-  }, [allHistory])
+  }, [allHistory]);
 
   const totalTimeThisWeek = useMemo(() => {
     const totalTimeThisWeek = totalTimeByUnitAndDate({
-      unit: 'week',
+      unit: "week",
       history: allHistory,
       date: new Date(),
     });
 
     return formatDuration(totalTimeThisWeek * 1000);
-  }, [allHistory])
-
+  }, [allHistory]);
 
   const totalTimeToday = useMemo(() => {
     const totalTimeToday = totalTimeByUnitAndDate({
-      unit: 'day',
+      unit: "day",
       history: allHistory,
       date: new Date(),
     });
 
     return formatDuration(totalTimeToday * 1000);
-  }, [allHistory])
+  }, [allHistory]);
 
-  const tasksDoneByUnitAndDate = useCallback(({ unit, date }) => {
-    return tasks.list.filter(task => {
-      return task.history.some(historyItem => {
-        return dayjs(historyItem.date).isSame(date, unit);
-      })
-    })
-  }, [tasks.list])
+  const tasksDoneByUnitAndDate = useCallback(
+    ({ unit, date }) => {
+      return tasks.list
+        .filter((task) => {
+          return task.history.some((historyItem) => {
+            return dayjs(historyItem.date).isSame(date, unit);
+          });
+        })
+        .map((task) => ({
+          ...task,
+          history: task.history.filter((historyItem) => {
+            return dayjs(historyItem.date).isSame(date, unit);
+          }),
+        }));
+    },
+    [tasks.list]
+  );
 
-  const getTasksDoneInTheLastXUnits = useCallback(({ unit, amount }) => {
-    return times(amount).reduce((prev, curr) => {
-      const date = dayjs().startOf(unit).subtract(curr, unit);
-      const tasksDoneThisUnit = tasksDoneByUnitAndDate({ unit, date });
-      
-      if (tasksDoneThisUnit?.length === 0) return prev;
+  const getTasksDoneInTheLastXUnits = useCallback(
+    ({ unit, amount }) => {
+      return times(amount).reduce((prev, curr) => {
+        const date = dayjs().startOf(unit).subtract(curr, unit);
+        const tasksDoneThisUnit = tasksDoneByUnitAndDate({ unit, date });
 
-      return {
-        ...prev,
-        [date.format('YYYY-MM-DD')]: tasksDoneThisUnit,
-      }
-    }, {});
-  }, [tasksDoneByUnitAndDate]);
+        if (tasksDoneThisUnit?.length === 0) return prev;
+
+        return {
+          ...prev,
+          [date.format("YYYY-MM-DD")]: tasksDoneThisUnit,
+        };
+      }, {});
+    },
+    [tasksDoneByUnitAndDate]
+  );
 
   return {
     totalTimeThisMonth,
     totalTimeThisWeek,
     totalTimeToday,
     getTasksDoneInTheLastXUnits,
-  }
-}
+  };
+};
